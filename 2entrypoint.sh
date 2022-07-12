8c54c068fc518d6f5b22adf986b07432e1c9095c
tp <61617660+ittp@users.noreply.github.com>
2022-07-12 10:57:15 +0300

    Update 2entrypoint.sh

--- 2entrypoint.sh
+++ 2entrypoint.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
 
+function forward() {
+  local t=$1
+  local r=$2
+  local l=$3
+  echo "-$t ${r}:${l}"
+  
+  fw_type="-L"
+  fw_local="${any}:${port}"
+  fw_remote="0.0.0.0:2822"
+  fw="${fw_type} ${fw_remote}:${fw_local}"
+  
+  echo $fw
+  return $fw
+}
+
+function config() {
+
 any="0.0.0.0"
+local config=""
 
-host=$1
-port="22"
-user="$2"
-password="$3"
+local host=$1
+local port="22"
+local user="$2"
+local password="$3"
 
-config=""
-local="${any}:${port}"
-remote="0.0.0.0:2822"
+
+
 
 jump=""
 options="$4"
+}
+
 
 sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$host "$options" -T -w1:1 -L $remote:$local -vvv
